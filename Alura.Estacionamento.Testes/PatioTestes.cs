5d03a46cdd526df5c55b5412f48f93432ccf042c
Daniel Gomes <daniel.ferreirasg@hotmail.com>
2023-10-08 19:56:47 -0300

    incluindo operador do patio.

--- Alura.Estacionamento.Testes/PatioTestes.cs
+++ Alura.Estacionamento.Testes/PatioTestes.cs
@@ -13,6 +13,7 @@ namespace Alura.Estacionamento.Testes
 	public class PatioTestes : IDisposable
 	{
 		private Veiculo veiculo;
+		private Operador operador;
 		public ITestOutputHelper SaidaConsoleTeste;
 
 		public PatioTestes(ITestOutputHelper _saidaConsoleTeste)
@@ -20,14 +21,19 @@ namespace Alura.Estacionamento.Testes
 			SaidaConsoleTeste = _saidaConsoleTeste;
 			SaidaConsoleTeste.WriteLine("Construtor invocado.");
 			veiculo = new Veiculo();
+			operador = new Operador();
+			operador.Nome = "Deodoro Fonseca";
 		}
 
 		[Fact]
 		public void ValidaFaturamentoDoEstacionamentoComUmVeiculo()
 		{
 			var estacionamento = new Patio();
+			//Operador operador = new Operador();
+			//operador.Nome = "Deodoro Fonseca";
 			//var veiculo = new Veiculo();
 
+			estacionamento.OperadorPatio = operador;
 			veiculo.Proprietario = "Daniel Gomes";
 			veiculo.Tipo = TipoVeiculo.Automovel;
 			veiculo.Cor = "Preto";
@@ -49,6 +55,7 @@ namespace Alura.Estacionamento.Testes
 		public void ValidaFaturamentoDoEstacionamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
 		{
 			var estacionamento = new Patio();
+			estacionamento.OperadorPatio = operador;
 			//var veiculo = new Veiculo();
 			veiculo.Proprietario = proprietario;
 			veiculo.Placa = placa;
@@ -68,6 +75,7 @@ namespace Alura.Estacionamento.Testes
 		public void LocalizaVeiculoNoPatioComBaseNoIdTicket(string proprietario, string placa, string cor, string modelo)
 		{
 			var estacionamento = new Patio();
+			estacionamento.OperadorPatio = operador;
 			//var veiculo = new Veiculo();
 			veiculo.Proprietario = proprietario;
 			veiculo.Placa = placa;
@@ -82,9 +90,10 @@ namespace Alura.Estacionamento.Testes
 		}
 
 		[Fact]
-		public void AlterarDadosVeiculoDoProprioVeiculo()
+		public void AlterarDadosDoProprioVeiculo()
 		{
 			var estacionamento = new Patio();
+			estacionamento.OperadorPatio = operador;
 			//var veiculo = new Veiculo();
 
 			veiculo.Proprietario = "Daniel Gomes";
--- Alura.Estacionamento/Alura.Estacionamento.Modelos/Patio.cs
+++ Alura.Estacionamento/Alura.Estacionamento.Modelos/Patio.cs
@@ -18,9 +18,13 @@ namespace Alura.Estacionamento.Modelos
         }
         private List<Veiculo> veiculos;
         private double faturado;
+        private Operador _operadorPatio;
+
         public double Faturado { get => faturado; set => faturado = value; }
-        public List<Veiculo> Veiculos { get => veiculos; set => veiculos = value; }       
-        public double TotalFaturado()
+        public List<Veiculo> Veiculos { get => veiculos; set => veiculos = value; }
+		public Operador OperadorPatio { get => _operadorPatio; set => _operadorPatio = value; }
+
+		public double TotalFaturado()
         {
             return this.Faturado;
         }
@@ -107,7 +111,8 @@ namespace Alura.Estacionamento.Modelos
             string ticket = "### Ticket Estacionamento Alura ###" +
                 $">>> Identificador: {veiculo.IdTicket}" +
                 $">>> Data/Hora de Entrada: {DateTime.Now}" +
-                $">>> Placa Veículo: {veiculo.Placa}";
+                $">>> Placa Veículo: {veiculo.Placa}" +
+                $">>> Operador Patio: {OperadorPatio.Nome}";
             veiculo.Ticket = ticket;
             return ticket;
 		}
